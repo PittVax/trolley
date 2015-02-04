f524f57b14dbe6219c351d77341b9b36642900b4
Jay DePasse <depasse@psc.edu>
2015-02-04 09:56:26 -0500

    added yaml config file parser

--- taoi/taoi.py
+++ taoi/taoi.py
@@ -22,20 +22,61 @@ except Exception as e:
 import argparse
 import logging
 log = logging.getLogger(__name__)
-
+import yaml
+import os
 
 class TaoiSession(object):
-    def __init__(self, host='localhost', treefile=None, workspace=None,
-            debug=False, auto=True):
-        if host != 'localhost':
-            msg = 'only local execution is supported'
-            log.error(msg)
-            raise Exception(msg)
-        self.workspace = workspace
-        self.treefile = treefile
-        if auto:
-            self.connect()
-            self.open_tree()
+    def __init__(self, host=None, treefile=None, workspace=None,
+            debug=None, auto=None, config=None):
+
+        self.configfile = config
+        self.config_workspace(config, workspace)
+
+#        self.debug = debug
+#        self.auto = auto
+#        
+#        if host != 'localhost':
+#            msg = 'only local execution is supported'
+#            log.error(msg)
+#            raise Exception(msg)
+#        
+#        self.set_workspace(workspace)
+#        self.set_treefile(treefile)
+#        
+#        if self.auto:
+#            self.connect()
+#            self.open_tree()
+
+    def config_workspace(self, config, workspace):
+        """ Reads the config and workspace options and sets class members """
+        try:
+            self.workspace = os.getcwd() if workspace is None else workspace
+            with open(os.path.join(self.workspace,config)) as f:
+                self.c = yaml.safe_load(f)
+            log.debug('successfully parsed yaml config file')
+        except Exception as e:
+            if workspace is None:
+                msg = 'No workspace provided and unable to parse supplied' \
+                    ' config file (%s)' % config
+                log.error(msg)
+                e.args += (msg,)
+                raise
+            else:
+                self.c = dict(workspace=self.workspace)
+                log.debug('attempting to use supplied workspace without yaml config')
+        finally:
+            if not os.access(self.workspace, os.R_OK):
+                msg = 'No read access to workspace path: %s' % self.workspace
+                log.error(msg)
+                raise Exception(msg)
+            if workspace is not None and workspace in self.c:
+                log.warn('workspace specified in config and as argument; '\
+                        'using supplied argument: %s' % workspace)
+        print(self.c)
+
+
+
+
     def connect(self):
         try:
             self.app = TreeAgeProApplication()
@@ -58,36 +99,41 @@ class TaoiSession(object):
                 raise Exception(msg)
         except JavaRemoteException as e:
             log.error('Unable to open tree %s' % _treefile)
+    def tree_summary(self):
+        pass
 
 def main():
 
     parser = argparse.ArgumentParser(description='TreeAge Object Interface Wrapper')
     
     parser.add_argument('-d', '--debug', action='store_true', default=False)
-    parser.add_argument('-t', '--tree',
+    parser.add_argument('-t', '--tree', default=None,
             help='Tree file (xml)')
     parser.add_argument('-H', '--host', default='localhost',
             help='Host running TreeAgePro')
-    parser.add_argument('-o', '--outdir', default='output',
+    parser.add_argument('-o', '--outdir', default=None,
             help='Output directory')
-    parser.add_argument('-w', '--workspace',
+    parser.add_argument('-w', '--workspace', default=None,
             help='Path prefix for workspace (defaults to current working directory)')
-    parser.add_argument('-p', '--prefix',
+    parser.add_argument('-p', '--prefix', default=None,
             help='Prefix used for all output files (defaults to a timestamp)')
+    parser.add_argument('-c', '--config', default=None,
+            help='YAML-format config file (command line args override config)')
+
 
     args = parser.parse_args()
 
     if args.debug:
-        logging.basicConfig(level=logging.INFO)
-    else:
         logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
     global log
     log = logging.getLogger('taoi_main')
 
     log.info(args)
     
-    ts = TaoiSession(args.host, args.tree, args.output_directory,
-            args.debug, auto=True)
+    ts = TaoiSession(args.host, args.tree, args.outdir,
+            debug=args.debug, auto=True, config=args.config)
 
 if __name__=="__main__":
     main()
