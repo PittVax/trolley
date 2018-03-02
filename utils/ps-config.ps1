182aac238246d0dd21a9d929ddd3114652f5a9c0
Jonathan Raviotta <jraviotta@gmail.com>
2018-03-02 17:45:44 -0500

    More docker edits and PS alias work.

--- utils/ps-config.ps1
+++ utils/ps-config.ps1
@@ -1,7 +1,8 @@
+$ScriptPath = Split-Path $MyInvocation.MyCommand.Path
 #Include docker aliases
-Import-Module .\docker_aliases.psm1
+Import-Module $ScriptPath\docker_aliases.psm1
 
 # Import Start-jython command
-Import-Module .\trolley_aliases.psm1
+Import-Module $ScriptPath\trolley_aliases.psm1
 
 
--- utils/trolley_aliases.psm1
+++ utils/trolley_aliases.psm1
@@ -2,7 +2,11 @@
 # All unsaved data will be lost
 # at the jython prompt, exit() exits the jython shell and returns to bash in the container
 
-# Launches jython interactive interpreter in containter
-function Start-jython {docker run -it --rm -v ${PWD}:/scratch -w /scratch mcandre/docker-jython:latest jython}
-# Launches bash shell in containter
-function Start-bash {docker run -it --name docker_jython -v ${PWD}:/scratch -w /scratch mcandre/docker-jython:latest }
+# Launches bash in jython containter
+function Start-Container {docker run -it --rm --name trolley --mount type=bind,source=$(pwd),target=/root/trolley --workdir="/root/trolley" pittvax/trolley:latest}
+
+# Launches jython in interactive mode in jython containter
+function Start-jython {docker run -it --rm --name trolley --mount type=bind,source=$(pwd),target=/root/trolley --workdir="/root/trolley" pittvax/trolley:latest jython}
+
+# Launches jython and runs trolley.py
+function Start-Trolley {docker run -it --rm --name trolley --mount type=bind,source=$(pwd),target=/root/trolley --workdir="/root/trolley" pittvax/trolley:latest jython taoi/trolley.py $args}
